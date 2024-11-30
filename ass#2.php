66709d4ff182ccd1a50d5298eb7c6964a3b9c7d4
abrarAlqh <202011998@stu.uob.edu.bh>
2024-11-30 17:34:50 +0300

    change

--- ass#2.php
+++ ass#2.php
@@ -1,32 +1,23 @@
 <?php 
 
-$url="https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
+$url = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
 
-$response= file_get_contents(filename: $url );
+$response = file_get_contents($url);
+$data = json_decode($response, true);
 
-echo $response;
-print_r(value: $response);
-
-$data=json_decode(json: $response, associative:  true);
-
-echo $response;
-print_r(value: $response);
-
-if (!$data ||  !isset($data["result"]))  {
-        die('error fetching the data from the api ');
-
+if (!$data || !isset($data["result"])) {
+    die('error fetching the data from the api ');
 }
-$result = $data["result"];
-print_r(value:$result);
 
- ?>
+$result = $data["result"];
+?>
 
 <html>
-    <head>
-        <title> </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-       <link rel="stylesheet" href="css/pico.min.css">
-       <style>
+<head>
+    <title>Student Statistics</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/pico.min.css">
+    <style>
         body {
             font-family: Arial, sans-serif;
             margin: 20px;
@@ -55,29 +46,29 @@ print_r(value:$result);
     </style>
 </head>
 <body>    
-    <thead>
-        <table>
-        <tr>
-            <th>Year </th>
-            <th>Semster  </th>
-            <th>The Program </th>
-            <th>Nationality</th>
-            <th>colleges </th>
-            <th>Number Of students </th>
-        </tr>
+    <table>
+        <thead>
+            <tr>
+                <th>Year</th>
+                <th>Semester</th>
+                <th>The Program</th>
+                <th>Nationality</th>
+                <th>Colleges</th>
+                <th>Number Of Students</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($result as $student): ?>
+                <tr>
+                    <td><?php echo htmlspecialchars($student["year"]); ?></td>
+                    <td><?php echo htmlspecialchars($student["semester"]); ?></td>
+                    <td><?php echo htmlspecialchars($student["the_programs"]); ?></td>
+                    <td><?php echo htmlspecialchars($student["nationality"]); ?></td>
+                    <td><?php echo htmlspecialchars($student["colleges"]); ?></td>
+                    <td><?php echo htmlspecialchars($student["number_of_students"]); ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
     </table>
-</thead>
-<tbody>
-    <?php
-    foreach($result as $student){  }
-    ?>
-    <tr>
-    <td> <?php  echo $student["year"]; ?> </td>
-    </tr>
-   
-
-    <?php
-    ?>
-</tbody>
-
+</body>
 </html>
